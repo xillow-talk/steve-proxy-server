f623d0d05265a20ce7b03a2164c91cf7e230c7c5
Steve Duong <steve@Plan.D>
2019-02-21 18:09:01 -0800

    refactors to pull from AWS

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use(morgan('dev'));
 console.log(__dirname);
 app.use(express.static(path.resolve(__dirname, 'public')));
 
+app.get('/:houseId', (req, res) => {
+  // let houseId = req.params.houseId;
+
+  res.sendFile(path.resolve(__dirname + '/public/index.html'));
+});
 app.listen(port, () => {
   console.log(`Proxy Server running at port ${port}`)
 });
