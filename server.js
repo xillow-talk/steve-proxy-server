63bdaf1e57960a61c54667a2b1c6cbd6fa01a0d3
Steve Duong <steve@Plan.D>
2019-02-21 21:03:40 -0800

    configuration for AWS

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const path = require('path');
 const morgan = require('morgan');
 
 const app = express();
-const port = 9000;
+const port = process.env.PORT || 9000;
 
 app.use(morgan('dev'));
 console.log(__dirname);
 app.use(express.static(path.resolve(__dirname, 'public')));
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get('/:houseId', (req, res) => {
   // let houseId = req.params.houseId;
 
